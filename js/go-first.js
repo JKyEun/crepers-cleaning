17d0607ee293f8917d1823a790f9c137f9b05437
JKyEun <j56237@naver.com>
2023-01-13 13:39:52 +0900

    feat: 처음으로 버튼 하나 더 생성

--- js/go-first.js
+++ js/go-first.js
@@ -5,4 +5,10 @@ function goFirst() {
     location.reload();
 }
 
+function goFirst2() {
+    localStorage.removeItem('studentsfin');
+    location.reload();
+}
+
 document.querySelector('#go-first').addEventListener('click', goFirst);
+document.querySelector('#go-first2').addEventListener('click', goFirst2);
